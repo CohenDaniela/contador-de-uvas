75a967cf60b92ecc4f2123d53edc8d835666c9eb
Daniela Cohen <danielacohen@Danielas-MacBook-Pro.local>
2023-12-28 17:16:47 +0100

    change footer

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# contador-de-uvas
+
+# Contador de Uvas 🍇🎉
+
+Este es un contador animado para celebrar el inicio del año nuevo. ¡Cuenta atrás para dar la bienvenida al 2024 con uvas!
+
+## Descripción
+
+Este proyecto es un contador interactivo que inicia desde 12 y cuenta hacia atrás hasta 0, mostrando uvas en cada conteo. Una vez que alcanza 0, despliega un mensaje de "Feliz 2024" con efectos de fuegos artificiales.
+
+## Características
+
+- **Contador Interactivo:** Comienza desde 12 y disminuye hasta 0, mostrando uvas.
+- **Animaciones de Fuegos Artificiales:** Despliega efectos visuales cuando alcanza el final del conteo.
+- **Reinicio Simple:** Botón que reinicia el contador para comenzar de nuevo.
+
+## Tecnologías Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Uso
+
+1. Clona este repositorio: `git clone https://github.com/tu-usuario/contador-de-uvas.git`
+2. Abre `index.html` en tu navegador.
+
+## Personalización
+
+Si deseas personalizar este contador para adaptarlo a tus propias celebraciones o eventos, puedes hacer lo siguiente:
+
+- **Cambio de Mensaje:** Modifica el mensaje que se muestra al final del conteo.
+- **Añadir más Elementos:** Agrega más elementos visuales o mensajes para hacerlo más festivo.
+
+## Contribuir
+
+¡Siéntete libre de contribuir a este proyecto! Puedes crear _pull requests_ con mejoras, solución de problemas o nuevas características.
+
+## Créditos
+
+Este proyecto fue creado por Daniela Cohen(https://github.com/CohenDaniela).
--- index.js
+++ index.js
@@ -3,86 +3,66 @@
 
 let counter = 12;
 let temp;
-const btn = document.querySelector('button')
-btn.innerHTML = 'Start'
-const listaUvas = document.querySelector('.uvas')
-// let container = '';
+const btn = document.querySelector('button');
+btn.innerHTML = 'Start';
+const listaUvas = document.querySelector('.uvas');
 const time = document.querySelector('.time');
-
 const header = document.querySelector('.titleTwo');
-const footer = document.querySelector('footer')
-const fireWork = document.querySelector('.firework')
-const fireWork2 = document.querySelector('.firework2')
-const fireWork3 = document.querySelector('.firework3')
-
-const main = document.querySelector('main')
-const headertitle = document.querySelector ('.headerTitle')
-
-
-
-
+const footer = document.querySelector('footer');
+const fireWork = document.querySelector('.firework');
+const fireWork2 = document.querySelector('.firework2');
+const fireWork3 = document.querySelector('.firework3');
+const main = document.querySelector('main');
+const headertitle = document.querySelector('.headerTitle');
 
 
 let uvas = ['🍇', '🍇', '🍇', '🍇', '🍇', '🍇',
-'🍇', '🍇', '🍇', '🍇', '🍇', '🍇', ];
+  '🍇', '🍇', '🍇', '🍇', '🍇', '🍇',];
 
 
-
-
-
-
-
-const renderList = ()=> {
+const renderList = () => {
   let container = '';
-    uvas.map((item)=> {container+=`<li> ${item}  </li>`})
-    listaUvas.innerHTML= container;
-    return container;
+  uvas.map((item) => { container += `<li> ${item}  </li>` })
+  listaUvas.innerHTML = container;
+  return container;
 }
 renderList();
 
 
 const showUvasAndCounter = () => {
- 
+
   header.classList.remove('color')
   btn.classList.add('hidden');
   listaUvas.classList.remove('hidden');
   time.classList.remove('hidden');
-  fireWork.classList.add('hidden')
-  fireWork2.classList.add('hidden')
-  fireWork3.classList.add('hidden')
-  
-  main.classList.remove('colorDark')
-  footer.classList.remove('colorDark')
-  header.classList.remove('colorDark')
-  headertitle.classList.remove('headertitle')
+  fireWork.classList.add('hidden');
+  fireWork2.classList.add('hidden');
+  fireWork3.classList.add('hidden');
+  main.classList.remove('colorDark');
+  footer.classList.remove('colorDark');
+  header.classList.remove('colorDark');
+  headertitle.classList.remove('headertitle');
 
- 
- 
+
+
   renderList();
 };
 
 const showFelizAno = () => {
-  
+
   listaUvas.classList.add('hidden');
   time.classList.add('hidden');
-  header.classList.add ('color');
-  btn.innerHTML= 'reset'
- 
-  fireWork.classList.remove('hidden')
-  fireWork2.classList.remove('hidden')
-  fireWork3.classList.remove('hidden')
-  
-  main.classList.add('colorDark')
+  header.classList.add('color');
+  btn.innerHTML = 'reset';
+  fireWork.classList.remove('hidden');;
+  fireWork2.classList.remove('hidden');
+  fireWork3.classList.remove('hidden');
+
+  main.classList.add('colorDark');
   btn.classList.remove('hidden');
-  footer.classList.add('colorDark')
-  header.classList.add('colorDark')
-  headertitle.classList.add(' headertitle')
-
- 
-
-
- 
-  
+  footer.classList.add('colorDark');
+  header.classList.add('colorDark');
+  headertitle.classList.add(' headertitle');
 };
 
 const handleClick = () => {
@@ -114,13 +94,13 @@ btn.addEventListener('click', handleClick);
 
 
 
-    
-          
-        
-          
-         
-          
-         
+
+
+
+
+
+
+
 
 
 
